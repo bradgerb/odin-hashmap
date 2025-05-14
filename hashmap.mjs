83c4188899cf77a94f270369ac860d0cad26d887
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-05-14 15:30:44 -0400

    Add modules and import linkedList from previous project

--- hashmap.mjs
+++ hashmap.mjs
@@ -1,3 +1,5 @@
+import LinkedList from "./linkedList.mjs";
+
 //Use when accessing bucket through an index
 // if (index < 0 || index >= buckets.length) {
 //     throw new Error("Trying to access index out of bounds");
@@ -6,7 +8,8 @@
 class HashMap {
     constructor (loadFactor, capacity) {
     this.loadFactor = loadFactor;
-    this.capacity = capacity;        
+    this.capacity = capacity;
+    this.storage = [];        
     }
 
     hash(key) {
@@ -19,4 +22,18 @@ class HashMap {
         
         return hashCode;
     }
+
+    // set(key, value) {
+
+    // }
 }
+
+const testHash = new HashMap(1, 16);
+
+console.log(testHash.hash("test"));
+
+
+const testList = new LinkedList();
+
+testList.append("turtle");
+console.log(testList.toString());
---
+++ linkedList.mjs
@@ -0,0 +1,127 @@
+class LinkedList {
+    constructor() {
+    this.head = null;
+    this.length = 0;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value, this.head);
+        this.head = newNode;
+        this.length++;
+    }
+
+    append(value) {
+        let currentNode = this.head;
+
+        if (this.head === null) {
+            this.prepend(value);
+        } else {
+
+        while (currentNode.next !== null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = new Node(value, null);
+        
+        this.length++;
+        }
+    }
+
+    size() {
+        return this.length
+    }
+
+    getHead() { 
+        return this.head
+    }
+
+    getTail() {
+        let currentNode = this.head;
+
+        while (currentNode.next !== null) {
+            currentNode = currentNode.next;
+        }
+
+        return currentNode
+    }
+
+    at(index) {
+        let currentNode = this.head;
+
+        for (let i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+
+        return currentNode
+    }
+
+    pop() {
+        let currentNode = this.head;
+
+        if (currentNode.next){
+
+            if(currentNode.next.next){
+                while (currentNode.next.next !== null) {
+                    currentNode = currentNode.next;
+                }
+                currentNode.next = null;
+                this.length --;
+            } else {
+                currentNode.next = null;
+                this.length --;
+            }
+
+        } else if (this.head){
+            this.head = null;
+            this.length --;
+        }
+    }
+
+    contains(value) {
+        let isTrue = false
+        let currentNode = this.head;
+
+        while (currentNode !== null) {
+            if (currentNode.value === value){
+                isTrue = true;
+            }
+            currentNode = currentNode.next;
+        }
+
+        return isTrue
+    }
+
+    find(value) {
+        let index;
+        let currentNode = this.head;
+
+            for (let i = 0; i < this.length; i++){
+                if (currentNode.value === value){
+                    index = i;
+                }
+                currentNode = currentNode.next;
+            }
+        
+        return index
+    }
+
+    toString() {
+        let currentNode = this.head;
+        let string = '';
+
+        while (currentNode !== null) {
+            string += `( ${currentNode.value} ) -> `;
+            currentNode = currentNode.next;
+        }
+        string += 'null';
+        return string
+    }
+}
+
+class Node {
+    constructor(value, nextNode) {
+      this.value = value;
+      this.next = nextNode;
+    }
+}
+
+export default LinkedList
